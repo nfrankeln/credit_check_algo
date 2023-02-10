df4c9a3d81e154f1316eb3b30effe2fbe3658d11
nfrankeln <ntnyfranky@gmail.com>
2023-02-10 15:00:54 -0600

    used map and reduce functions

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,17 @@
-exports.creditCheck = function(str) {
+module.exports = creditCheck = (creditNumber) => {
+creditNumber=creditNumber.toString().split("")
+creditNumber = creditNumber.map((numChar,index) => {
+    numDigit = Number(numChar);
+    if (index % 2 === 0){
+        numDigit *= 2
+        if (numDigit  > 9){
+            numChar = numDigit.toString().split("")
+            numDigit = Number(numChar[0])+Number(numChar[1])
+        }
+    } 
+    return numDigit
+});
+sum = creditNumber.reduce((total, current)=> total + current)
+if (sum % 10 ===0) return "The number is valid!"
+return "The number is invalid!"
 }
--- js/creditCheckSpec.js
+++ js/creditCheckSpec.js
@@ -2,10 +2,10 @@
 
 var cc = require("./creditCheck");
 
-console.log(cc.creditCheck('5541808923795240') === "The number is valid!");
-console.log(cc.creditCheck("4024007136512380") === "The number is valid!");
-console.log(cc.creditCheck("6011797668867828") === "The number is valid!");
+console.log(cc('5541808923795240') === "The number is valid!");
+console.log(cc("402400713651380") === "The number is valid!");
+console.log(cc("6011797668867828") === "The number is valid!");
 
-console.log(cc.creditCheck("5541801923795240") === "The number is invalid!");
-console.log(cc.creditCheck("4024007106512380") === "The number is invalid!");
-console.log(cc.creditCheck("6011797668868728") === "The number is invalid!");
+console.log(cc("5541801923795240") === "The number is invalid!");
+console.log(cc("4024007106512380") === "The number is invalid!");
+console.log(cc("6011797668868728") === "The number is invalid!");
